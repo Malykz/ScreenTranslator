2a68c1e6a5f431e0d0d1f1138ab7be5df7a73825
luthfi <luthlirik@gmail.com>
2025-02-04 16:53:01 +0700

    save image to temp

--- src/converter.py
+++ src/converter.py
@@ -3,13 +3,24 @@ import pyautogui
 from PIL import Image
 import translate
 import getScreen
+import os
+
+
+def image_temp_location(filename) -> str :
+    script_location = os.path.dirname(os.path.relpath(__file__))
+    temp_location = os.path.join(script_location, "temp")
+    image_location = os.path.join(temp_location, filename)
+
+    return image_location
 
 def image_convert(cordinate: dict) -> str:
+    image_location = image_temp_location("iyakah.png")
+
     x, y, width, height = cordinate
-    pyautogui.screenshot().save("iyakah.png")
+    pyautogui.screenshot().save(image_location)
 
     try :
-        image = Image.open("iyakah.png").crop((x, y, x + width, y + height))
+        image = Image.open(image_location).crop((x, y, x + width, y + height))
 
     except Exception:
         raise ValueError("Be sure you were pointing correctly.")
